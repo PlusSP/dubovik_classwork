53fa6dffad6aaadd14cfcc98005a4310a5a3675a
Pavel Dubovik <33573891+PlusSP@users.noreply.github.com>
2017-11-14 01:48:42 +0300

    new .gitignore

---
+++ slasswork2/src/classwork2.java
@@ -0,0 +1,47 @@
+public class classwork2 {
+
+    public static void main(String[] arguments) {
+
+
+        int[] array1 = new int[5];
+        array1[0] = 3;
+        array1[1] = 2;
+        array1[2] = 1;
+        array1[3] = 4;
+        array1[4] = 5;
+
+//        int min,max;
+//        double avg;
+//        min = array1[0];
+//        for (int i = 1; i < array1.length; i++){
+//            if(min > array1[i]){
+//              min = array1[i];
+//            }
+//        }
+//        System.out.println(min);
+//        for (int i = array1.length - 1; i >= 0; i--) {
+//            System.out.println("i= " + i);
+//            System.out.println("array " + array1[i]);
+//        }
+//
+//        for (int value : array1) { //сокращённая форма For
+//            System.out.println(value);
+//        }
+//        int i = 0;
+//        while(i < array1.length){
+//           System.out.println(array1[i]);
+//           i++;
+//        }
+//        do {
+//            System.out.println(array1[i]);
+//        } while (false);
+
+        //приведение типов
+//        double a = 5.5;
+//        int b = (int)a;
+//        // или
+//        String text = "100";
+//        int i = Integer.valueOf(text);
+//        System.out.println(b+i);
+    }
+}
